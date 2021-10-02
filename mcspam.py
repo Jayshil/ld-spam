bb843500982ecf4ccd25a83cafd2bf2bc2153051
Jayshil <jayshilpatel147@gmail.com>
2021-10-02 18:04:27 +0530

    Imported lsq

--- mcspam.py
+++ mcspam.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize as mz
 from scipy.optimize import curve_fit as cft
+from scipy.optimize import leastsq as lsq
 from tqdm import tqdm
 import batman
 
