ac721aaea53af795bc013da2fc95d6673775e70a
Jayshil <jayshilpatel147@gmail.com>
2021-11-09 23:04:05 +0530

    Wrote functions to made teff vs residuals plots

--- utils.py
+++ utils.py
@@ -78,7 +78,7 @@ def average_resid(diff, diffe):
     return med8, std8
 
 
-def image_double(xdata1, xdata2, xerr1, xerr2, ydata1, ydata2, yerr1, yerr2, label1, label2, xlabel, ylabel, ttl, lim1=None, lim2=None):
+def image_double(xdata1, xdata2, xerr1, xerr2, ydata1, ydata2, yerr1, yerr2, label1, label2, xlabel, ylabel, ttl, lim1=None, lim2=None, diff_ret=False):
     """
     Function to make figures according
     to Patel & Espinoza (2021) pattern
@@ -189,9 +189,13 @@ def image_double(xdata1, xdata2, xerr1, xerr2, ydata1, ydata2, yerr1, yerr2, lab
 
     plt.subplots_adjust(hspace = 0.3)
 
-    med1, std1 = average_resid(diff_1, diff_1e)
-    med2, std2 = average_resid(diff_2, diff_2e)
-    return (med1, std1), (med2, std2)
+    if diff_ret:
+        return diff_1, diff_1e, diff_2, diff_2e
+    else:
+        med1, std1 = average_resid(diff_1, diff_1e)
+        med2, std2 = average_resid(diff_2, diff_2e)
+        return (med1, std1), (med2, std2)
+        
 
 
 def single_image(xdata, xerr, ydata, yerr, xlabel, ylabel):
@@ -289,3 +293,275 @@ def limiting_ldcs(c1, c2, c3, c4):
     u1 = (12./35.)*c1 + c2 + (164./105.)*c3 + 2.*c4
     u2 = (10./21.)*c1 - (34./63.)*c3 - c4
     return u1, u2
+
+#---------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------
+
+def lowest_bic(a):
+    """
+    Find the lowest BIC (Bayesian Inference Crterian) among
+    three BICs.
+    I know I can just look at them manually but I am a little-
+    bit lazy to do so. Hence here I am with a function defined for it.
+    Parameters
+    ----------
+    a: dict
+        Values of BICs in dict.
+    returns: dict
+        Containing the best fit model, with its BIC.
+        If two models have BIC difference less than 2,
+        then the model with a fewer number of parameters
+        would be selected.
+    """
+    b = np.array([])
+    for i in a.values():
+        b = np.hstack((b,i))
+    def get_key(val, my_dict):
+        for key, value in my_dict.items():
+            if val == value:
+                return key
+        return "Key does not exist"
+    xx = np.min(b)
+    yy = get_key(xx, a)
+    ret = {}
+    con = np.abs(a[yy] - a['constant'])
+    lin = np.abs(a[yy] - a['linear'])
+    qua = np.abs(a[yy] - a['quadratic'])
+    if yy == 'quadratic':
+        if con < 2:
+            ret['constant'] = a['constant']
+        elif lin < 2 and lin < con:
+            ret['linear'] = a['linear']
+        else:
+            ret['quadratic'] = a['quadratic']
+    if yy == 'linear':
+        if con < 2:
+            ret['constant'] = a['constant']
+        else:
+            ret['linear'] = a['linear']
+    if yy == 'constant':
+        ret['constant'] = a['constant']
+    return ret
+
+#---------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------
+
+def binned_data(datax, datay, nos=10, datax_err=None, datay_err=None):
+    """
+    This function creates binned array from the given array.
+    Parameters
+    ----------
+    datax: np.ndarray
+        One dimensional array. x-coordinate
+    datax_err: np.ndarray
+        One dimensional array. Error in x-coordinate
+        If not provided then assumes to be a zero matrix
+        of length of datax.
+    datay: np.ndarray
+        One dimensional array. y-coordinate
+    datay_err: np.ndarray
+        One dimensional array. Error in y-coordinate
+        If not provided then assumes to be a zero matrix
+        of length of datay.
+    nos: float
+        Number of binned data points you want to set.
+        Default is 10.
+    returns: {np.ndarray}
+        numpy array of binned data
+    """
+    if datax_err is None:
+        datax_err = np.zeros(len(datax))
+    if datay_err is None:
+        datay_err = np.zeros(len(datay))
+    aa = []
+    for i in range(len(datax)):
+        xxx = (datax[i], datax_err[i], datay[i], datay_err[i])
+        aa.append(xxx)
+    bb = sorted(aa)
+    aaa = bbb = ccc = ddd = np.array([])
+    for i in range(len(bb)):
+        aaa = np.hstack((aaa, bb[i][0]))
+        bbb = np.hstack((bbb, bb[i][1]))
+        ccc = np.hstack((ccc, bb[i][2]))
+        ddd = np.hstack((ddd, bb[i][3]))
+    rep = int((len(datax))/(nos-1))
+    rem = len(datax) - ((nos-1)*rep)
+    bin_datax = bin_datax_err = bin_datay = bin_datay_err = np.array([])
+    k = 0
+    for i in range(nos-1):
+        du_t = np.zeros(1000)
+        for j in range(rep):
+            abc1 = np.random.normal(aaa[k], bbb[k], 1000)
+            du_t = np.vstack((du_t, abc1))
+            k = k+1
+        bint = np.mean(du_t[1:], axis=0)
+        bin_datax = np.hstack((bin_datax, np.median(bint)))
+        bin_datax_err = np.hstack((bin_datax_err, np.std(bint)))
+    rem_t = np.zeros(1000)
+    for i in range(rem):
+        abc1 = np.random.normal(aaa[k], bbb[k], 1000)
+        rem_t = np.vstack((rem_t, abc1))
+    remt = np.mean(rem_t[1:], axis=0)
+    bin_datax = np.hstack((bin_datax, np.median(remt)))
+    bin_datax_err = np.hstack((bin_datax_err, np.std(remt)))
+    k1 = 0
+    for i in range(nos-1):
+        du_d = np.zeros(1000)
+        for j in range(rep):
+            abc1 = np.random.normal(ccc[k1], ddd[k1], 1000)
+            du_d = np.vstack((du_d, abc1))
+            k1 = k1+1
+        bind = np.mean(du_d[1:], axis=0)
+        bin_datay = np.hstack((bin_datay, np.median(bind)))
+        bin_datay_err = np.hstack((bin_datay_err, np.std(bind)))
+    rem_d = np.zeros(1000)
+    for i in range(rem):
+        abc1 = np.random.normal(ccc[k1], ddd[k1], 1000)
+        rem_d = np.vstack((rem_d, abc1))
+    remd = np.mean(rem_d[1:], axis=0)
+    bin_datay = np.hstack((bin_datay, np.median(remd)))
+    bin_datay_err = np.hstack((bin_datay_err, np.std(remd)))
+    return bin_datax, bin_datax_err, bin_datay, bin_datay_err
+
+def teff_vs_resid(teff, xdata1, xerr1, ydata1, yerr1, xdata2, xerr2, ydata2, yerr2, label1, label2, ylabel):
+    """
+    Function to make plots of effective temps
+    vs residuals of Empirical LDCs - Theoretical LDCs
+    -------------------------------------------------
+    Parameters:
+    -----------
+    teff : numpy.ndarray
+        Effective temperatures of the host stars
+    xdata1, ydata1 : numpy.ndarray
+        x-data and y-data of the first kind of residuals
+    xerr1, yerr1 : numpy.ndarray
+        errors on xdata1 and ydata1
+    xdata2, ydata2 : numpy.ndarray
+        x-data and y-data of the second kind of residuals
+    xerr2, yerr2 : numpy.ndarray
+        errors on xdata2 and ydata2
+    label1, label2 : str
+        Labels of both plots
+    xlabel, ylabel : str
+        xlabels and ylabels of the plot
+    -----------
+    return
+    -----------
+    matplotlib.figure
+    """
+    resid1, resid1_err, resid2, resid2_err = image_double(xdata1, xdata2, xerr1, xerr2, ydata1, ydata2, yerr1,\
+         yerr2, label1, label2, xlabel, ylabel, ttl='', lim1=None, lim2=None, diff_ret=True)
+    # Basics
+    tmin = np.min(teff)
+    tmax = np.max(teff)
+
+    t11 = np.linspace(tmin, tmax, 1000)
+
+    x = np.linspace(tmin, tmax, 100)
+    y = np.zeros(len(x))
+
+    def line(x,m,c):
+        function = m*x + c
+        return function
+
+    def constant(x, c):
+        function = c + x*0
+        return function
+
+    def quadratic(x, a, b, c):
+        function = a*x*x + b*x + c
+        return function
+
+    model = ['constant', 'linear', 'quadratic']
+    
+    # Making a plot and plotting the points
+    fig1 = plt.figure(figsize=(8,6))
+
+    plt.errorbar(teff, resid1, yerr = resid1_err, fmt='.', elinewidth=1, alpha=0.35, color='orangered', zorder=5, label = label1)
+    plt.errorbar(teff, resid2, yerr = resid2_err, fmt='.', elinewidth=1, alpha=0.35, color='cornflowerblue',zorder=5, label = label2)
+
+    #-----Plotting binned data
+    bin_teff, bin_teffe, bin_diff1, bin_diff1e = binned_data(datax=teff, datay=resid1, datay_err=resid1_err)
+    plt.errorbar(bin_teff, bin_diff1, xerr=bin_teffe, yerr=bin_diff1e, fmt='o',\
+        color='orangered', mew=2, ms=8, alpha=1, markerfacecolor='white', mec='orangered', zorder=10)
+
+    bin_teff, bin_teffe, bin_diff2, bin_diff2e = binned_data(datax=teff, datay=resid2, datay_err=resid2_err)
+    plt.errorbar(bin_teff, bin_diff2, xerr=bin_teffe, yerr=bin_diff2e, fmt='o',\
+        color='cornflowerblue', mew=2, alpha=1, ms=8, markerfacecolor='white', mec='cornflowerblue', zorder=10)
+
+    #----Constant fitting
+    popt_c, pcov_c = cft(constant, teff, resid1)
+    popt1_c, pcov1_c = cft(constant, teff, resid2)
+
+    rss_c = rss1_c = 0
+    for i in range(len(teff)):
+        r111 = (resid1[i] - constant(teff[i], *popt_c))**2
+        rss_c = rss_c + r111
+        r222 = (resid2[i] - constant(teff[i], *popt1_c))**2
+        rss1_c = rss1_c + r222
+
+    bic_c = len(resid1)*np.log((rss_c)/(len(resid1))) + np.log(len(resid1))
+    bic1_c = len(resid2)*np.log((rss1_c)/(len(resid2))) + np.log(len(resid2))
+
+    #----Linear fitting
+    popt_l, pcov_l = cft(line, teff, resid1)
+    popt1_l, pcov1_l = cft(line, teff, resid2)
+
+    rss_l = rss1_l = 0
+    for i in range(len(teff)):
+        r111 = (resid1[i] - line(teff[i], *popt_l))**2
+        rss_l = rss_l + r111
+        r222 = (resid2[i] - line(teff[i], *popt1_l))**2
+        rss1_l = rss1_l + r222
+
+    bic_l = len(resid1)*np.log((rss_l)/(len(resid1))) + 2*np.log(len(resid1))
+    bic1_l = len(resid2)*np.log((rss1_l)/(len(resid2))) + 2*np.log(len(resid2))
+
+    #----Quadratic fitting
+    popt_q, pcov_q = cft(quadratic, teff, resid1)
+    popt1_q, pcov1_q = cft(quadratic, teff, resid2)
+
+    rss_q = rss1_q = 0
+    for i in range(len(teff)):
+        r111 = (resid1[i] - quadratic(teff[i], *popt_q))**2
+        rss_q = rss_q + r111
+        r222 = (resid2[i] - quadratic(teff[i], *popt1_q))**2
+        rss1_q = rss1_q + r222
+
+    bic_q = len(resid1)*np.log((rss_q)/(len(resid1))) + 3*np.log(len(resid1))
+    bic1_q = len(resid2)*np.log((rss1_q)/(len(resid2))) + 3*np.log(len(resid2))
+
+    #----Plotting models
+    bic = [bic_c, bic_l, bic_q]
+    bic1 = [bic1_c, bic1_l, bic1_q]
+
+    dict_bic = {}
+    dict_bic1 = {}
+    for i in range(3):
+        dict_bic[model[i]] = bic[i]
+        dict_bic1[model[i]] = bic1[i]
+
+    best_bic = utl.lowest_bic(dict_bic)
+    if 'constant' in best_bic:
+        plt.plot(t11, constant(t11, *popt_c), color = 'orangered', ls = '-.', zorder=3, alpha=0.85)
+    elif 'linear' in best_bic:
+        plt.plot(t11, line(t11, *popt_l), color='orangered', ls='-.', zorder=3, alpha=0.85)
+    elif 'quadratic' in best_bic:
+        plt.plot(t11, quadratic(t11, *popt_q), color = 'orangered', ls='-.', zorder=3, alpha=0.85)
+
+    best_bic1 = utl.lowest_bic(dict_bic1)
+    if 'constant' in best_bic1:
+        plt.plot(t11, constant(t11, *popt1_c), color = 'cornflowerblue', ls = '-.', zorder=3, alpha=0.85)
+    elif 'linear' in best_bic1:
+        plt.plot(t11, line(t11, *popt1_l), color='cornflowerblue', ls='-.', zorder=3, alpha=0.85)
+    elif 'quadratic' in best_bic1:
+        plt.plot(t11, quadratic(t11, *popt1_q), color = 'cornflowerblue', ls='-.', zorder=3, alpha=0.85)
+
+    #---------------------
+    plt.plot(x, y, 'k--')
+    plt.grid()
+    plt.legend(loc='best')
+    plt.ylabel(ylabel)
+    plt.xlabel('Effective Temperature')
