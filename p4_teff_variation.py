7cfd2ca2eff386d75ad5313f82b0b12a240db215
Jayshil <jayshilpatel147@gmail.com>
2021-11-10 13:53:05 +0530

    Teff vs resid complete

--- p4_teff_variation.py
+++ p4_teff_variation.py
@@ -23,6 +23,7 @@ rcParams['ytick.direction'] = 'out'
 
 # Path to the data (i.e., SPAM LDCs)
 p1d = os.getcwd() + '/Results_New/'
+p2d = os.getcwd() + '/resid_vs_teff/'
 
 # Getting data from our modelling
 path1 = "/home/jayshil/Documents/Dissertation/ld-project-updated"
@@ -51,6 +52,39 @@ l1, l2 = -0.4, 1.4
 
 # Just remember to remove TOI-157b from teff1 and u1_j, u2_j arrays
 
-utl.teff_vs_resid(teff=teff1, xdata1=u1_j, xerr1=u1_jp, ydata1=u1a_c17, yerr1=errs, xdata2=u1_j,\
-      xerr2=u1_jp, ydata2=u1p_c17, yerr2=errs, label1='PHOENIX LDCs', label2='ATLAS LDCs', ylabel=r'Residuals in $u_1$ from Claret (2017)')
-plt.show()
+# u1_cla_te
+utl.teff_vs_resid(teff=teff1, xdata1=u1_j, xerr1=u1_jp, ydata1=u1p_c17, yerr1=errs, xdata2=u1_j,\
+      xerr2=u1_jp, ydata2=u1a_c17, yerr2=errs, label1='PHOENIX SPAM LDCs', label2='ATLAS SPAM LDCs', ylabel=r'Residuals in $u_1$ Claret (2017) LDCs')
+plt.savefig(p2d + 'u1_cla_te_SPAM.pdf')
+#plt.show()
+
+# u1_cla_te_r
+utl.teff_vs_resid(teff=teff1, xdata1=u1_j, xerr1=u1_jp, ydata1=u1p_c17, yerr1=errs, xdata2=u1_j,\
+      xerr2=u1_jp, ydata2=u1p_c17r, yerr2=errs, label1='PHOENIX SPAM LDCs - q method', label2='PHOENIX SPAM LDCs - r method', ylabel=r'Residuals in $u_1$ Claret (2017) LDCs')
+plt.savefig(p2d + 'u1_cla_te_r_SPAM.pdf')
+#plt.show()
+
+# u1_code_te
+utl.teff_vs_resid(teff=teff1, xdata1=u1_j, xerr1=u1_jp, ydata1=u1p_ej15, yerr1=errs, xdata2=u1_j,\
+      xerr2=u1_jp, ydata2=u1a_ej15, yerr2=errs, label1='PHOENIX SPAM LDCs - q method', label2='ATLAS SPAM LDCs - r method', ylabel=r'Residuals in $u_1$ Espinoza \& Jordan (2015) LDCs')
+plt.savefig(p2d + 'u1_code_te_SPAM.pdf')
+#plt.show()
+
+
+# u2_cla_te
+utl.teff_vs_resid(teff=teff1, xdata1=u2_j, xerr1=u2_jp, ydata1=u2p_c17, yerr1=errs, xdata2=u2_j,\
+      xerr2=u2_jp, ydata2=u2a_c17, yerr2=errs, label1='PHOENIX SPAM LDCs', label2='ATLAS SPAM LDCs', ylabel=r'Residuals in $u_2$ Claret (2017) LDCs')
+plt.savefig(p2d + 'u2_cla_te_SPAM.pdf')
+#plt.show()
+
+# u2_cla_te_r
+utl.teff_vs_resid(teff=teff1, xdata1=u2_j, xerr1=u2_jp, ydata1=u2p_c17, yerr1=errs, xdata2=u2_j,\
+      xerr2=u2_jp, ydata2=u2p_c17r, yerr2=errs, label1='PHOENIX SPAM LDCs - q method', label2='PHOENIX SPAM LDCs - r method', ylabel=r'Residuals in $u_2$ Claret (2017) LDCs')
+plt.savefig(p2d + 'u2_cla_te_r_SPAM.pdf')
+#plt.show()
+
+# u2_code_te
+utl.teff_vs_resid(teff=teff1, xdata1=u2_j, xerr1=u2_jp, ydata1=u2p_ej15, yerr1=errs, xdata2=u2_j,\
+      xerr2=u2_jp, ydata2=u2a_ej15, yerr2=errs, label1='PHOENIX SPAM LDCs - q method', label2='ATLAS SPAM LDCs - r method', ylabel=r'Residuals in $u_2$ Espinoza \& Jordan (2015) LDCs')
+plt.savefig(p2d + 'u2_code_te_SPAM.pdf')
+#plt.show()
